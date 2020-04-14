a28e923ee375ffa621cd38ec842c5311425c15c2
Capidunord <stephanecapdevielle@gmail.com>
2020-04-14 11:51:18 +0200

    Using Lualatex now.

--- Debian_install/install.sh
+++ Debian_install/install.sh
@@ -6,8 +6,9 @@ apt install -y texlive-full
 apt install -y git python3 python3-pip
 python3 -m pip install sympy pygments
 cd ..
+rm -rf /texlab
 mkdir /texlab
 cp -r . /texlab
-cp /texlab/latexmk/.latexmkrc-docker /home/stephane/.latexmkrc
-cp /texlab/latexindent/latexindentconfig.yaml /home/stephane/latexindentconfig.yaml
-cp /texlab/latexindent/indentconfig_wsl.yaml /home/stephane/indentconfig.yaml
+cp -f /texlab/latexmk/.latexmkrc-docker /home/stephane/.latexmkrc
+cp -f /texlab/latexindent/latexindentconfig.yaml /home/stephane/latexindentconfig.yaml
+cp -f /texlab/latexindent/indentconfig_wsl.yaml /home/stephane/indentconfig.yaml
--- latexmk/.latexmkrc
+++ latexmk/.latexmkrc
@@ -1,5 +1,4 @@
-$pdf_mode = 5;
-$xelatex = 'xelatex -no-pdf %O -shell-escape -synctex=1 %S';
+$xelatex = 'lualatex -no-pdf %O -shell-escape -synctex=1 %S';
 $recorder = 1;
 add_cus_dep('pytxcode', 'tex', 0, 'pythontex');
 sub pythontex { return system("pythontex \"$_[0]\""); }
